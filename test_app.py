c6df793d3c3d4891411d99adfa5fbf2e89080283
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-29 03:05:20 +0200

    Basic test

---
+++ test_app.py
@@ -0,0 +1,25 @@
+import unittest
+from app import app, todos
+
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+    def test_create_todo(self):
+        response = self.app.post(
+            "/create",
+            data=dict(title="Test Todo", description="This is a test todo"),
+            follow_redirects=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        #self.assertIn(b"Test Todo", response.data)
+        #self.assertIn(b"This is a test todo", response.data)
+
+    def tearDown(self):
+        todos.clear()
+
+
+if __name__ == "__main__":
+    unittest.main()
